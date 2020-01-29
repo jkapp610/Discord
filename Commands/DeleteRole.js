044cbe55ec47b3eecbbffff4d3649e3dc1d9ccbe
jkapp610 <jkapp@umich.edu>
2020-01-28 20:53:21 -0500

    Deleate fixed

--- Commands/DeleteRole.js
+++ Commands/DeleteRole.js
@@ -1,11 +1,13 @@
 module.exports = {
     //name of the command
-    name: "Delete Role",
+    name: "DeleteRole",
     //description of the command
     description: "Delete role from channel",
     //execute is the function that will run when the file is called
     execute(message,args){
+        //delete role
         message.guild.roles.find(rol => rol.name === args[1]).delete();
+       
        
     }
 
