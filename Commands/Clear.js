5ccc3a34624a4355c6981a6a2f1cdb08e6ab7a49
Rekkur150 <minecraftzachary150@gmail.com>
2020-03-17 14:36:25 -0400

    Added QA suggestions and fix some problems with !help, !clear. Suggest making changes to deletechannel command to fix issues with user accidently delete channel their in when expecting it to delete another channel

--- Commands/Clear.js
+++ Commands/Clear.js
@@ -5,34 +5,28 @@ module.exports = {
     description: "clears messages from the channel given an argument",
      //execute is the function that will run when the file is called
     execute(message,args){
-         //if there is no 2nd argument 
+         //if there is no 2nd argument
         if(!args[1]){
-            // output error message 
+            // output error message
             return message.reply('Error Please enter two arguments');
         }
         //if the 2nd arument is not a number
          else if(isNaN(args[1])){
-            //bot gives error message to 
+            //bot gives error message to
             return message.reply('Error please enter a number!!');
     }
-    else{   
+    else{
         // set the number of deleate  to +1
         var numdel = parseInt(args[1])+1;
-         // deleate the number of messageges 
-        message.channel.bulkDelete(numdel);
+         // deleate the number of messageges
+
+        if (numdel > 0 && numdel < 100) {           //QA-ZACH: Added if statement to prevent bot from crashing when enterting a value less than 0 and a value greater than or equal to 0
+          message.channel.bulkDelete(numdel);
+        } else {
+          return message.rply('Error please enter a valid number between 0,100'); //QA-ZACH: added a error message to handle error
+        }
         }
     }
-    
+
 
 }
-
-
-
-
-
-
-
-
-
-
-
--- Commands/Help.js
+++ Commands/Help.js
@@ -10,15 +10,15 @@ module.exports = {
     description: "creates an enbed in discord",
     // execute is the function that will be ran when the file is called
     execute(message,args){
-       
+
 
         Commands("Create Role","!CreateRole Cat red","Takes the name of the role and a color");
-        Commands("Delete Role","!DelRole Cat","Takes the name if the role to delete");
+        Commands("Delete Role","!DeleteRole Cat","Takes the name if the role to delete");         //QA-ZACH: Modified values to represent that actual command
         Commands("RemoveRole","!RemoveRole Cat","Takes the name of the role");
         Commands("Add Role","!AddRole Cat","Adds the role of the user who typed the command");
         Commands("Clear","!Clear 10","Takes the number of message they want to delete");
         Commands("Create Channel","!CreateChannel helloworld","Takes the channel name");
-        Commands("Delete Channel","DelChannel","Deleate channel where the message is sent from\n(if i want to delete the helloworld cannel type this command in that channel)")
+        Commands("Delete Channel","DeleteChannel","Delete channel where the message is sent from\n(if i want to delete the helloworld cannel type this command in that channel)") //QA-ZACH: Modified values to represent that actual command
         function Commands(title,Example,des){
             const embed = new Discord.RichEmbed()
              //creates a title for the embed
@@ -28,9 +28,8 @@ module.exports = {
              .setColor(0xF1C40F);
              // bot outputs the embed message to the channel
              message.channel.sendEmbed(embed);
-        
+
         }
     }
 
 }
-
