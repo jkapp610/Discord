d94c3d1775900fc25eb34f252cf6dc17509030e0
jkapp610 <jkapp@umich.edu>
2020-03-19 12:19:50 -0400

    update

--- Commands/DeleteChannel.js
+++ Commands/DeleteChannel.js
@@ -8,12 +8,24 @@ module.exports = {
     //execute is the function that gets ran when the file is called
     execute(message,args){
 
-        
-        // call the Deleate role
-        message.channel.delete()
-        // outputs error message that the role was created
-        .then(role => console.log('Channel Deleted'))
-        .catch(console.error)
+        //check to see if there is a second argument if not outputs error
+        if(!args[1]){
+
+            return message.reply('Error Please enter two arguments');
+        }
+        //sets the channel name passed in by user
+        let mychannel = message.guild.channels.find(ch => ch.name ===args[1]);
+        // if channel exists 
+        if(mychannel != null){
+            // call the Deleate role
+            mychannel.delete()
+             // outputs error message that the role was created
+            .then(role => console.log('Channel Deleted'))
+            .catch(console.error)
+        }
+        else{
+            message.channel.send(`Error there is no channel named ${args[1]}`);
+        }
 
     }
 
